41c8255e0e6a35cfcf9c0f39735351c4bd9052a3
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-05-05 16:02:47 +0530

    Refactor: Make the home content fully dynamic

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,56 @@
 import dinerImage from "../images/diner.jpg";
-import activeButtonToggler from "./active-button-toggler.js";
 
 const loadHomePage = function () {
   const mainContainer = document.getElementById("content");
-  const templateContent = document.getElementById("home");
-  const homeContent = templateContent.content.cloneNode(true);
-  const imageElement = homeContent.querySelector(".image");
-  imageElement.src = dinerImage;
+  const homeContent = makeOuterContainer();
 
-  const homeButton = document.querySelector(".nav__home");
-  activeButtonToggler(homeButton);
   mainContainer.appendChild(homeContent);
+};
+
+const makeOuterContainer = () => {
+  const container = document.createElement("div");
+  container.classList.add("container", "home");
+
+  const leftContainer = makeLeftContainer();
+  const rightContainer = makeRightContainer();
+
+  container.appendChild(leftContainer);
+  container.appendChild(rightContainer);
+
+  return container;
+};
+
+const makeLeftContainer = () => {
+  const leftContainer = document.createElement("div");
+  leftContainer.classList.add("content__left");
+
+  const headerSecondLevel = document.createElement("h2");
+  headerSecondLevel.classList.add("content__title");
+  headerSecondLevel.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("content__body");
+  paragraph.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque velit nam amet, beatae, cumque cupiditate optio esse earum nostrum quae doloremque sequi! Pariatur reprehenderit totam quo dignissimos porro voluptates alias";
+
+  leftContainer.appendChild(headerSecondLevel);
+  leftContainer.appendChild(paragraph);
+
+  return leftContainer;
+};
+
+const makeRightContainer = () => {
+  const rightContainer = document.createElement("div");
+  rightContainer.classList.add("content__right");
+
+  const imageElement = document.createElement("img");
+  imageElement.src = dinerImage;
+  imageElement.alt = "A diner";
+
+  rightContainer.appendChild(imageElement);
+
+  return rightContainer;
 };
 
 export { loadHomePage };
